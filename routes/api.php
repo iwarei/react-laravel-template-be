5e4329bb10c0c9522225ce4f73b0d9c1fe06fa1c
iwarei <jjjjjjjjjjjjjjjjj93@gmail.com>
2023-08-19 00:04:08 +0900

    [FIX] Route定義整理

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use App\Http\Controllers\Auth;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// 認証関連(ログイン前)
+Route::middleware('guest')->group(function () {
+    Route::post('/register', [Auth\RegisteredUserController::class, 'store'])->name('register');
+    Route::post('/login', [Auth\AuthenticatedSessionController::class, 'store'])->name('login');
+    Route::post('/forgot-password', [Auth\PasswordResetLinkController::class, 'store'])->name('password.email');
+    Route::post('/reset-password', [Auth\NewPasswordController::class, 'store'])->name('password.store');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/verify-email/{id}/{hash}', Auth\VerifyEmailController::class)
+                    ->middleware(['signed', 'throttle:6,1'])
+                    ->name('verification.verify');
+    
+    Route::post('/email/verification-notification', [Auth\EmailVerificationNotificationController::class, 'store'])
+                    ->middleware('throttle:6,1')
+                    ->name('verification.send');
+    Route::post('/logout', [Auth\AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
+});
+
